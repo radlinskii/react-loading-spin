2a518725f49655fcd6fbd0464fe91239739edf23
radlinskii <radlinsk@student.agh.edu.pl>
2018-04-24 11:22:56 +0200

    :zap: destructuring props

--- src/index.js
+++ src/index.js
@@ -2,52 +2,32 @@ import React from 'react';
 import './styles.css';
 
 const LoadingSpin = (props) => {
-    let size = '60px';
-    let width = '6px';
-    let secondaryColor = '#cccccc';
-    let primaryColor = '#1ecd96';
-    let duration = '1.4s';
-    let timingFunction = 'ease-in-out';
-    let direction = 'normal';
-
-    if(props.size){
-        size = props.size;
-    }
-    if(props.width){
-        width = props.width;
-    }
-    if(props.duration){
-        duration = props.duration;
-    }
-    if(props.timingFunction){
-        timingFunction = props.timingFunction;
-    }
-    if(props.direction){
-        direction = props.direction;
-    }
-    if(props.secondaryColor){
-        secondaryColor = props.secondaryColor;
-    }
-    if(props.primaryColor){
-        primaryColor = props.primaryColor;
-    }
+    const {
+        size = '60px',
+        width = '6px',
+        secondaryColor = '#cccccc',
+        primaryColor = '#1ecd96',
+        duration = '1.4s',
+        timingFunction = 'ease-in-out',
+        direction = 'normal',
+    } = props;
 
     return (
         <div
             className="loading"
-            style = {
-            {
-                height: size,
-                width: size,
-                borderWidth: width,
-                animationDuration: duration,
-                animationTimingFunction: timingFunction,
-                animationDirection: direction,
-                borderColor: secondaryColor,
-                borderLeftColor: primaryColor,
-                borderTopColor: primaryColor,
-            }
-        }></div>
+            style={
+                {
+                    height: size,
+                    width: size,
+                    borderWidth: width,
+                    animationDuration: duration,
+                    animationTimingFunction: timingFunction,
+                    animationDirection: direction,
+                    borderColor: secondaryColor,
+                    borderLeftColor: primaryColor,
+                    borderTopColor: primaryColor,
+                }
+            }></div>
     );
 };
 
