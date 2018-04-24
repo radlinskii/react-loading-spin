3c7826abceec1dc9b2af230783d310afea65b8e2
radlinskii <radlinsk@student.agh.edu.pl>
2018-04-24 09:42:43 +0200

    :zap: adding styling via props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import './styles.css';
 
-const LoadingSpin = () => {
+const LoadingSpin = (props) => {
+    let primaryColor = '#1ecd96';
+    let secondaryColor = '#cccccc';
+    if(props.secondaryColor){
+        secondaryColor = props.secondaryColor;
+    }
+    if(props.primaryColor){
+        primaryColor = props.primaryColor;
+    }
+
     return (
-        <div className="loading"></div>
+        <div
+            className="loading"
+            style = {
+            {
+                borderColor: secondaryColor,
+                borderLeftColor: primaryColor,
+                borderTopColor: primaryColor,
+            }
+        }></div>
     );
 };
 
