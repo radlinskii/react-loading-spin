a4557c5fcddc24ff7a1c15892b43605be990d122
radlinskii <radlinsk@student.agh.edu.pl>
2018-04-24 19:08:56 +0200

    :art: clean files up

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 
-const SRC = path.resolve(__dirname, "src");
-const OUTPUT = path.resolve(__dirname, "lib");
+const SRC = path.resolve(__dirname, 'src');
+const OUTPUT = path.resolve(__dirname, 'lib');
 
 const config = {
     entry: {
-        Index: SRC + "/index.js"
+        Index: `${SRC}/index.js`
     },
 
     output: {
         path: OUTPUT,
-        filename: "index.js",
-        libraryTarget: "umd"
+        filename: 'index.js',
+        libraryTarget: 'umd'
     },
 
     devtool: 'source-map',
@@ -33,7 +33,7 @@ const config = {
                 include: SRC,
                 test: /\.css$/,
                 exclude: /(node_modules|lib)/,
-                use: ["style-loader", "css-loader"],
+                use: ['style-loader', 'css-loader'],
             }
         ]
     },
