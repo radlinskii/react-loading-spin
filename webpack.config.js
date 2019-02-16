1577eadb94be7fc5754e5d9f5214e33ec7645717
radlinskii <radlinskiignacy@gmail.com>
2019-02-16 16:27:54 +0100

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ const config = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'react']
+                        presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
             }, {
